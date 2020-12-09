887bf207b45bcc0beb15457178751d07b651e5d3
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-12-09 17:39:46 +0530

    Integral Knapsack Input documented

--- src/Container/Input/IntegralKnapSackInput.jsx
+++ src/Container/Input/IntegralKnapSackInput.jsx
@@ -1,12 +1,23 @@
+
+/**
+ * @author Drisya P
+ */
 import React, { Component } from "react";
 import "../../Utils/Button.css"
 import "../../Utils/Label.css"
 
-export default class IntegralKnapSackInput extends Component {
+/**
+ * @component IntegralKnapSackInput
+ * @description Class component that takes care of Integral KnapSack algorithm input
+ * constructor receives props from the parent component
+ * sets intial states needed for IN(Integral Knapsack) algorithm and binds event handlers.
+ * @listens props.func
+ */
+class IntegralKnapSackInput extends Component {
 
     /**
-     * Input form for the Integral knapsack problem , which accepts value, weight of the item and capacity of the knapsack.
-     * @param {object} props  includes the function that has to be called in main class after the form is submitted 
+     * @constructor 
+     * @param {function} props Contains a function (which helps to start visualisation) that is called when the input form is submitted.
      */
     constructor(props) {
         super(props);
@@ -22,7 +33,10 @@ export default class IntegralKnapSackInput extends Component {
 
 
     /**
-     * Helps in dynamic change in the input form 
+     * @function
+     * @event 
+     * @param {object} contains details (value, name etc )about an input field
+     * @description Whenever we change an input field , corresponding state of that entity changes.
      */
     handleChange(event) {
 
@@ -30,11 +44,17 @@ export default class IntegralKnapSackInput extends Component {
         this.setState({ [name]: value });
     }
 
-    /**
-     * Input values converted to array of integers, by splitting using comma.
-     */
-    inputProcess() {
 
+    /**
+     * @function
+     * @description Function to preprocess the input given by User. For Integral Knapsack inputs are given by comman seperated.
+     * This function split the input and make it in to array. And all the input is wrapped in to an object called 'data'
+     */
+    inputPreProcess() {
+
+        /**
+         * Extracting input values
+         */
         let temp_values = this.state.values.split(',');
         let temp_weights = this.state.weights.split(',');
         const values = temp_values.map((num) => { return Number(num) });
@@ -42,6 +62,9 @@ export default class IntegralKnapSackInput extends Component {
         const number = values.length;
         const capacity = Number(this.state.Capacity)
 
+        /**
+         * Wrapping up weight, value, capacity of knapsack in to an object.
+         */
         const data = {
             values: values,
             weights: weights,
@@ -52,15 +75,21 @@ export default class IntegralKnapSackInput extends Component {
     }
 
     /**
-     * After submition of the form , parent class function is called by passing
-     * a signal (boolen variable) and the necessary data
+     * @function
+     * @event
+     * @param {object} event All information about the input field
+     * @description function 'inputPreProcess' is called when the input form is submitted.
      */
     handleSubmit(event) {
 
-        this.props.func(true, this.inputProcess())
+        this.props.func(true, this.inputPreProcess())
         event.preventDefault();
     }
 
+    /**
+     * @function
+     * @description ReactJS **Lifecycle Method** helps to render the Input form in the UI.
+     */
     render() {
 
         return (
@@ -91,3 +120,5 @@ export default class IntegralKnapSackInput extends Component {
         )
     }
 }
+
+export default IntegralKnapSackInput;
