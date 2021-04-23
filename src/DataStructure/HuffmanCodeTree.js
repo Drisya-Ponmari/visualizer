571275d52b2d65fe784bc42795d2cb8738cdb19e
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2021-04-24 02:02:21 +0530

    label showing condition added

--- src/DataStructure/HuffmanCodeTree.js
+++ src/DataStructure/HuffmanCodeTree.js
@@ -8,15 +8,15 @@ export default function HuffmanCodeTree(surface, data) {
 
     var group = new Group();
     var nodeCoordinates = [];
-    var radius = 30;
-
+    var radius = 30, color = 'green', edgeColor = 'black';
+    var left, right;
     var cx = 100, cy = 100, inc = 150, cir, center;
 
-    console.log(cx, cy);
+
     for (let i = 0; i < data.leaves; i++) {
 
         center = [cx, cy];
-        cir = SmallCircle(center, data.chars[i] + "::" + data.values[i], radius,);
+        cir = SmallCircle(center, data.chars[i] + "::" + data.values[i], radius, color);
         group.append(cir);
         nodeCoordinates.push(center);
         cx += inc;
@@ -24,19 +24,28 @@ export default function HuffmanCodeTree(surface, data) {
 
     for (let i = data.leaves; i < data.vertices; i++) {
 
+        if (i == data.vertices - 1)
+            edgeColor = 'red';
         let p1 = nodeCoordinates[data.edges[i][0]];
         let p2 = nodeCoordinates[data.edges[i][1]];
 
         cx = (p1[0] + p2[0]) / 2;
+        if (p1[0] < p2[0]) {
+            left = 1;
+            right = 0;
+        } else {
+            right = 1;
+            left = 0;
+        }
         cy = Math.max(p1[1], p2[1]) + (inc / 2);
 
         center = [cx, cy];
-        cir = SmallCircle(center, data.values[i], radius);
+        cir = SmallCircle(center, data.values[i], radius, color);
         nodeCoordinates.push(center);
         group.append(cir);
 
-        let e1 = NewEdge(p1, center, 'black', true);
-        let e2 = NewEdge(p2, center, 'black', true);
+        let e1 = NewEdge(p1, center, edgeColor, data.label, left);
+        let e2 = NewEdge(p2, center, edgeColor, data.label, right);
 
         group.append(e1);
         group.append(e2);
